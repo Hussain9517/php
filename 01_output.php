df4955014140b916a0a6a652b1fe1a8eb5b74530
MUHAMMAD ALI HUSSAIN <80855891+Hussain9517@users.noreply.github.com>
2022-10-19 06:45:46 -0700

    Update 01_output.php

--- 01_output.php
+++ 01_output.php
@@ -7,43 +7,31 @@
   <title>Document</title>
 </head>
 <body>
-
+  
 <?php
  echo "Hey! My Name is Muhammad Ali Hussain."
 ?>
-
 <br>
-
 <?php
  echo "I'm 27 years old";
 ?>
-
 <br>
-
 <?php 
  echo 17 , "Oct", 1995;
 ?>
-
 <br>
-
 <?php
  print "How are you?"
 ?>
-
 <br>
-
 <?php
  print_r([1,2,3,4]);
 ?>
-
 <br>
-
 <?php
  var_dump("Muhammad Ali Hussain");
 ?>
-
 <br>
-
 <?php
  var_export("Muhammad Ali Hussain");
 ?>
